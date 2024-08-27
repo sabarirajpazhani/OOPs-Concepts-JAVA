a4e5a93aa8a3bbe24f9540e777736c9ec6dcde78
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-08-27 19:08:33 +0530

    with return type

---
+++ Methods/Methods2.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class Methods2 {
+    public static int add(int x1,int x2){
+        return x1+x2;
+    }
+
+    public static void main(String args[]){
+        int n1=10;
+        int n2=20;
+        int result = add(n1,n2);
+        System.out.println(result);
+    }
+}
