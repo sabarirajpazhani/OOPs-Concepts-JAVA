1365b940ab744b68f8bb38afa7ecb6081590f786
sabarirajpazhani <sabarirajpazhani@gmail.com>
2024-08-27 19:06:19 +0530

    first commit

---
+++ Methods/Methods.java
@@ -0,0 +1,11 @@
+package Methods;            // function without return 
+public class Methods{
+    public static void add(int x, int y){
+        System.out.print(x+y);
+    }
+    public static void main(String args[]){
+        int a = 10;
+        int b = 2;
+        add(a,b);
+    }
+}
